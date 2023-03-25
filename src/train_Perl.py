ec5988f0abe83c62576372d8d54065a46906df75
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-03-25 21:38:05 +0300

    refac : change hyperparameters

--- src/train_Perl.py
+++ src/train_Perl.py
@@ -12,32 +12,33 @@ device = torch.device('cuda:0')
 
 parser = argparse.ArgumentParser(description='FedPerl')
 
-parser.add_argument('--data_path', default='', type=str, metavar='PATH',
+parser.add_argument('--data_path', default='./dataset/', type=str, metavar='PATH',
                     help='path to the dataset(validation or testing)')
-parser.add_argument('--clients_path', default='', type=str, metavar='PATH',
+parser.add_argument('--clients_path', default='./dataset/clients/', type=str, metavar='PATH',
                     help='path to clients')
-parser.add_argument('--check_folder', default='', type=str, metavar='PATH',
+parser.add_argument('--check_folder', default='checkpoint', type=str, metavar='PATH',
                     help='path to the check points model')
-parser.add_argument('--clients_state', default='', type=str, metavar='PATH',
+parser.add_argument('--clients_state', default='states', type=str, metavar='PATH',
                     help='path to save the clients state temporarly')
-parser.add_argument('--models_folder', default='', type=str, metavar='PATH',
+parser.add_argument('--models_folder', default='models', type=str, metavar='PATH',
                     help='path to save the trained models')
 parser.add_argument('--model_name', default='FedPerl', type=str)
-parser.add_argument('--num_rounds', type=int, default=200)
-parser.add_argument('--steps', type=int, default=100, help='number of iteration')
+parser.add_argument('--num_rounds', type=int, default=2)
+parser.add_argument('--steps', type=int, default=10, help='number of iteration')
 parser.add_argument('--curr_lr', type=int, default=0.00005)
-parser.add_argument('--batch_size', type=int, default=16)
-parser.add_argument('--num_classes', type=int, default=8)
+parser.add_argument('--batch_size', type=int, default=5)
+parser.add_argument('--num_classes', type=int, default=6)
+parser.add_argument('--n_classes', type=int, default=6)
 parser.add_argument('--weight_decay', type=int, default=0.002)
 parser.add_argument('--img_channel', type=int, default=1)
-parser.add_argument('--img_width', type=int, default=256)
-parser.add_argument('--img_height', type=int, default=256)
+parser.add_argument('--img_width', type=int, default=224)
+parser.add_argument('--img_height', type=int, default=224)
 parser.add_argument('--alpha', type=int, default=1)
 parser.add_argument('--con', type=int, default=0.6, help='confidence treashold')
 parser.add_argument('--lambda_a', type=int, default=0.5, help='unlabeled coefficient')
 parser.add_argument('--lambda_i', type=int, default=0.01, help='consistency coefficient')
 parser.add_argument('--num_clients', type=int, default=10, help='num of clients')
-parser.add_argument('--connected_clients', type=int, default=10, help='connected clients')
+parser.add_argument('--connected_clients', type=int, default=2, help='connected clients')
 parser.add_argument('--num_peers', type=int, default=2, help='number of peers used in peer learning')
 parser.add_argument('--method', default='Perl', type=str,
                     help='current options Perl, Random, FedMatch, FixMatch')
