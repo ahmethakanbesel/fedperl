9daa678cd23c25debb23f8d4e1a90c68a6669e64
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-07 21:38:33 +0300

    feat: get_model, densenet and efficientnet

--- src/modules/models.py
+++ src/modules/models.py
@@ -1,6 +1,16 @@
+import torchvision
 from efficientnet_pytorch import EfficientNet
 
 
 def get_model(num_classes: int):
+    return efficientnet(num_classes)
+
+
+def efficientnet(num_classes: int):
     model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=num_classes)
+    return model
+
+
+def densenet(num_classes: int):
+    model = torchvision.models.densenet121(pretrained=True, num_classes=num_classes)
     return model
