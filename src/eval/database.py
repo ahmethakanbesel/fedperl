068892d0eeddbdd746f35e8637d72ebd5fb8e04c
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-17 23:10:49 +0300

    feat: use sqlite db for storing model results

---
+++ src/eval/database.py
@@ -0,0 +1,44 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file, check_same_thread):
+    """
+        Create a database connection to a SQLite database
+        SQLite in Python is multithreading safe.
+        It allows multiple threads to access the database simultaneously without causing any conflicts or issues.
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file, check_same_thread=check_same_thread)
+        conn.execute("PRAGMA foreign_keys = 1")
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+
+
+class Database:
+
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.conn = create_connection(self.db_file, False)
+        pass
+
+    def get_connection(self):
+        return self.conn
+
+    def insert_result(self, model: str, accuracy: float, f1: float, precision: float, recall: float, dataset: str):
+        query = 'INSERT INTO results (model, accuracy, f1, precision, recall, dataset) VALUES (?, ?, ?, ?, ?, ?)'
+        try:
+            # Execute the query, passing in the values
+            cursor = self.conn.execute(query, (model, accuracy, f1, precision, recall, dataset))
+            # Save the changes to the database
+            self.conn.commit()
+            # Retrieve the id of the inserted row
+            rowid = cursor.lastrowid
+            return rowid
+        except sqlite3.IntegrityError as error:
+            # Handle the UNIQUE constraint error
+            print('Error:', error)
+            return None
