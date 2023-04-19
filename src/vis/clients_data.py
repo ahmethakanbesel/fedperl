8187bdc44b04822900f2cbdf639376d96b0dae4c
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-19 12:06:57 +0300

    refactor: dynamic data counts

--- src/vis/clients_data.py
+++ src/vis/clients_data.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from src.modules.settings import DATASET
 
 # Define the number of clients and their respective data counts
 clients = ['client0', 'client1', 'client2', 'client3', 'client4', 'client5', 'client6', 'client7', 'client8', 'client9']
-labeled_counts = [100, 150, 200, 50, 120, 180, 90, 200, 100, 250]
-unlabeled_counts = [200, 250, 100, 150, 180, 300, 250, 100, 200, 150]
-validation_counts = [] # List to store sample validation data counts
+labeled_counts = []
+unlabeled_counts = []
+validation_counts = []
 
 # Loop through each client and generate sample validation data counts
 for i, client in enumerate(clients):
-    # Generate sample validation data counts
-    validation_counts.append(np.random.randint(1, 30, size=1)[0]) # replace with actual validation data counts
+    labeled, unlabeled, validation = DATASET.get_client_data_counts(i)
+    labeled_counts.append(labeled)
+    unlabeled_counts.append(unlabeled)
+    validation_counts.append(validation)
 
 # Create stacked bar chart
 fig, ax = plt.subplots()
@@ -26,9 +29,9 @@ ax.bar(clients, unlabeled_counts, width, bottom=labeled_counts, label='Unlabeled
 ax.bar(clients, validation_counts, width, bottom=np.array(labeled_counts) + np.array(unlabeled_counts), label='Validation Data')
 
 # Add labels and legend
-ax.set_ylabel('Data Counts')
+ax.set_ylabel('Image Counts')
 ax.set_xlabel('Clients')
-ax.set_title('Dataset of Each Client')
+#ax.set_title('Data Distribution Among the Clients')
 ax.legend()
 
 # Show the plot
