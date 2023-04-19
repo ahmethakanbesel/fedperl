387a88ac93d6e4fa17897ae6fa15a4ae7822c494
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-19 11:07:00 +0300

    refactor: change dataset path

--- src/modules/settings.py
+++ src/modules/settings.py
@@ -1,4 +1,7 @@
+import os
 from src.data.brain import BrainDataset
 
-DATA_PATH = '../../dataset/'
-DATASET = BrainDataset(DATA_PATH + '/dataset_img.npy', DATA_PATH + '/dataset_lbl.npy')
+DATA_PATH = '../dataset/'
+IMG_PATH = os.path.abspath(DATA_PATH + 'brain_dataset_img.npy')
+LABEL_PATH = os.path.abspath(DATA_PATH + 'brain_dataset_lbl.npy')
+DATASET = BrainDataset(IMG_PATH, LABEL_PATH)
