f89764ec9a1507f446d5785f0443e17d3b986593
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-21 23:44:27 +0300

    fix: missing class

--- src/vis/clients_classes.py
+++ src/vis/clients_classes.py
@@ -3,9 +3,10 @@ import numpy as np
 from src.modules.settings import DATASET
 
 # Define the number of clients and their respective class distributions
-clients = ['client0', 'client1', 'client2', 'client3', 'client4', 'client5', 'client6', 'client7', 'client8', 'client9']
+clients = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9']
 class_names = DATASET.classes
 class_distributions = [[] for i in range(len(class_names))]
+# class_distributions = []
 
 
 # Loop through each client and generate sample validation data counts
@@ -15,25 +16,19 @@ for i, client in enumerate(clients):
     for j, label in enumerate(client_classes):
         class_distributions[j].append(client_classes[label])
         client_dist.append(client_classes[label])
-    #class_distributions.append(client_dist)
-
-print(class_distributions)
 
 # Create stacked bar chart
 fig, ax = plt.subplots()
-width = 0.35
+width = 0.25
 
-for i, label in enumerate(class_names):
+for i in range(len(class_names)):
     # Plot labeled data counts
-    if i == 0:
-        ax.bar(clients, class_distributions[i], width, label=label)
-    else:
-        ax.bar(clients, class_distributions[i], width, bottom=class_distributions[i-1], label=label)
+    ax.bar(clients, class_distributions[i], label=class_names[i], bottom=np.sum(class_distributions[:i], axis=0))
 
 # Add labels and legend
 ax.set_ylabel('Image Counts')
 ax.set_xlabel('Clients')
-#ax.set_title('Class Distribution Among the Clients')
+# ax.set_title('Class Distribution Among the Clients')
 ax.legend()
 
 # Show the plot
