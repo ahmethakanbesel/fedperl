6c60610eee4c9a9523023c2b291eaf0c5632f2ef
Ahmet Hakan <ahmethakan.besel@gmail.com>
2023-04-23 17:59:05 +0300

    feat: support for legacy efficientnet

--- src/modules/models.py
+++ src/modules/models.py
@@ -1,12 +1,21 @@
 import torchvision
+from torch import nn
+from efficientnet_pytorch import EfficientNet
 
 
 def get_model(num_classes: int):
-    return densenet(num_classes)
+    return efficientnet_legacy(num_classes)
 
 
 def efficientnet(num_classes: int):
     model = torchvision.models.efficientnet_b0(weights=None, num_classes=num_classes)
+    return model
+
+
+def efficientnet_legacy(num_classes: int):
+    model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=num_classes)
+    num_ftrs = model._fc.in_features
+    model._fc = nn.Linear(num_ftrs, num_classes)
     return model
 
 
